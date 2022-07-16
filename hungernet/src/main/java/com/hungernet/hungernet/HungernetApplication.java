859b5875166b7653cdb5768b06af579c16958f11
emarini2022 <ermir.marini128@gmail.com>
2022-07-16 21:17:00 +0200

    shto

---
+++ hungernet/src/main/java/com/hungernet/hungernet/HungernetApplication.java
@@ -0,0 +1,13 @@
+package com.hungernet.hungernet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HungernetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HungernetApplication.class, args);
+	}
+
+}
---
+++ hungernet/src/test/java/com/hungernet/hungernet/HungernetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hungernet.hungernet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HungernetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
